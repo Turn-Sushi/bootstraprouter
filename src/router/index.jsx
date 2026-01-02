212d9c628f91849a6945fcd7532b4f421aa1d173
frydaynar <nara1743@gmail.com>
2026-01-02 12:13:21 +0900

    Notfound추가

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -4,6 +4,7 @@
 // import { createBrowserRouter } from "react-router";
 // import { RouterProvider } from "react-router/dom";
 import { BrowserRouter, Routes, Route } from "react-router";
+import NotFound from "@pages/NotFound.jsx"
 import Page1 from '@pages/Page1.jsx';
 import Page2 from '@pages/Page2.jsx';
 import Page3 from '@pages/Page3.jsx';
@@ -47,7 +48,7 @@ const Router = () => {
         <Route path="/page2" element={<Page2 />} />
         <Route path="/page3" element={<Page3 />} />
         <Route path="/page4" element={<Page4 />} />
-        <Route path="/page5" element={<Page5 />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   )
