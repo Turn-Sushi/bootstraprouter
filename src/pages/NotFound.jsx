dae91c023741f9841a7aea59eb3e438a68781b0c
frydaynar <nara1743@gmail.com>
2026-01-02 12:17:17 +0900

    수정

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+// NotFound
+// 보통 해당되지 않는 페이지를 제어하기 위해 만듬
+
+const NotFound = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>404</h1>
+      <p>페이지를 찾을 수 없습니다.</p>
+    </div>
+  )
+}
+
+export default NotFound
